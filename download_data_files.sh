14768477e15e000ab10d28fea31dfff2603ba842
Ethan <ethan.g.pierce@dartmouth.edu>
2025-08-30 14:59:58 -0600

    Add basalmelt download

--- download_data_files.sh
+++ download_data_files.sh
@@ -4,13 +4,9 @@ echo "machine urs.earthdata.nasa.gov login $EARTHDATA_USERNAME password $EARTHDA
 chmod 600 .netrc
 wget -P data/ https://data.nsidc.earthdatacloud.nasa.gov/nsidc-cumulus-prod-protected/ICEBRIDGE/IDBMG4/5/1993/01/01/BedMachineGreenland-v5.nc
 
-echo "Downloading GBaTSv2-GBaTSv2.zip"
-wget -P data/ https://zenodo.org/records/5714527/files/joemacgregor/GBaTSv2-GBaTSv2.zip
-mkdir data/GBaTSv2
-unzip data/GBaTSv2-GBaTSv2.zip -d data/GBaTSv2/
-mv data/GBaTSv2/joemacgregor-GBaTSv2-879adcb/GBaTSv2.nc data/GBaTSv2.nc
-rm -rf data/GBaTSv2-GBaTSv2.zip
-rm -rf data/GBaTSv2
+echo "Downloading basal melt rates from Karlsson et al. (2021)"
+wget -P data/ https://dataverse.geus.dk/api/access/datafile/19458?gbrecs=true
+mv data/19458?gbrecs=true data/basalmelt.nc
 
 echo "Downloading MEaSUREs_120m.nc"
 wget -P data/ https://its-live-data.s3.amazonaws.com/velocity_mosaic/v2/static/ITS_LIVE_velocity_120m_RGI05A_0000_v02.nc
